60c5e0aa68a931cc0d7fc2311aa01c0d6368d5ff
Sayyed Rushwan Ali <93130553+Rushwan07@users.noreply.github.com>
2025-03-05 16:15:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(express.json({ limit: "50mb" }));
 
 const allowedOrigins = [
   "http://localhost:5173",
+  "https://eventhub-frontend-xi.vercel.app",
   "https://main--extraordinary-fox-28d76d.netlify.app",
 ];
 
