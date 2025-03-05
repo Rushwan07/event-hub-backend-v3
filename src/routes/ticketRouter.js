80261f45ab3228145067cb51a9fe25210e4f89cc
Rushwan07 <rushdeveloper07@gmail.com>
2025-03-05 14:42:56 +0530

    BAckend

--- src/routes/ticketRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-const ticketController = require("../controllers/ticketController");
-
-router.post("/bookings", ticketController.bookTickets);
-router.get("/types/:eventId", ticketController.getTicketTypes);
-
-module.exports = router;
